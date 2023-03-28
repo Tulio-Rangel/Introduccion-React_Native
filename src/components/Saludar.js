bfadfa2bd6817e8a67c0da341911df0a8f1a9d01
Tulio-Rangel <tjrn20@hotmail.com>
2023-03-28 15:44:51 -0500

    default props

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -7,6 +7,12 @@ export default function Saludar(props) {
   return <Text>Hola que tal {name} </Text>;
 }
 
+//Forma con defaultProps
+// Saludar.defaultProps = {
+//   firstname: "Agustin",
+//   lastname: "Navarro Galdon",
+// };
+
 /**
  * Una forma mas simple de pasarle la propiedad
  * import { Text } from 'react-native'
