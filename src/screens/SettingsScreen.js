5ce82f1efd29a74dd8d65314bcb9b8a73929a441
Tulio-Rangel <tjrn20@hotmail.com>
2023-03-28 17:06:43 -0500

    Botones para navegar

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 
-export default function SettingsScreen() {
+export default function SettingsScreen(props) {
+  const { navigation } = props;
+
+  const goToPage = (pageName) => {
+    navigation.navigate(pageName);
+  };
+
   return (
     <View>
       <Text>SettingsScreen</Text>
+      <Button onPress={() => goToPage("Home")} title="Ir al Home" />
     </View>
   );
 }
